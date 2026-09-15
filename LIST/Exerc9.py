549e3b4bfb0c687808a302b101cf495e870bead3
Videirafoo <videirafo@gmail.com>
2026-09-14 23:00:12 -0300

    fix: corrige execução individual do exercício 9

--- LIST/Exerc9.py
+++ LIST/Exerc9.py
@@ -1,22 +1,26 @@
 def exerc09():
     print()
-
-    print("9. Inserção e remoção")  
+    print("9. Inserção e remoção")
     print("Crie uma lista inicialmente vazia. Leia 5 números e utilize append() para inseri-los.")
-    print("Depois, solicite um número ao usuário e remova sua primeira ocorrência, caso exista. Exiba a lista antes e depois da remoção.")
+    print("Depois, solicite um número ao usuário e remova sua primeira ocorrência, caso exista.")
+    print("Exiba a lista antes e depois da remoção.")
 
     print()
+
     lista = []
     for _ in range(5):
-        num = int(input("Digite um número: "))
-        lista.append(num)
+        numero = int(input("Digite um número: "))
+        lista.append(numero)
+
     print(f"Lista antes da remoção: {lista}")
-    num_remover = int(input("Digite o número a ser removido: "))
-    if num_remover in lista:
-        lista.remove(num_remover)
+    numero_remover = int(input("Digite o número a ser removido: "))
+
+    if numero_remover in lista:
+        lista.remove(numero_remover)
         print(f"Lista depois da remoção: {lista}")
     else:
         print("Número não encontrado na lista.")
 
-if __name__ == "__Main__":
+
+if __name__ == "__main__":
     exerc09()
