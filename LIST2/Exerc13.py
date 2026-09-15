3a89145e81b229f1d3328dab2f2944826bce0c0b
Videirafoo <videirafo@gmail.com>
2026-09-14 23:00:53 -0300

    fix: corrige execução individual do exercício 13

--- LIST2/Exerc13.py
+++ LIST2/Exerc13.py
@@ -1,17 +1,15 @@
-def exerc13 ():
-    
+def exerc13():
     print()
-    
     print("13. Valores acima da média")
-    print("Leia 10 números reais e armazene-os em uma lista.") 
+    print("Leia 10 números reais e armazene-os em uma lista.")
     print("Calcule a média e depois exiba somente os valores que ficaram acima da média.")
-          
+
     print()
-    
+
     numeros = []
     total = 0
 
-    for i in range(10):
+    for _ in range(10):
         numero = float(input("Digite um número: "))
         numeros.append(numero)
         total += numero
@@ -23,7 +21,9 @@ def exerc13 ():
     for numero in numeros:
         if numero > media:
             print(numero)
+
     print()
 
-if __name__ == "__Main__":
+
+if __name__ == "__main__":
     exerc13()
