607c40967bee03219c972d0acc5de6c5c54b253f
Videirafoo <videirafo@gmail.com>
2026-09-14 22:59:23 -0300

    fix: corrige execução individual do exercício 4

--- LIST/Exerc4.py
+++ LIST/Exerc4.py
@@ -1,5 +1,4 @@
 def exerc04():
-    
     print()
     print("4. Média de uma lista")
     print("Considere notas = [7.5, 8.0, 6.0, 9.5, 5.5].")
@@ -7,11 +6,16 @@ def exerc04():
     print("informe a média com duas casas decimais.")
 
     print()
+
     notas = [7.5, 8.0, 6.0, 9.5, 5.5]
     soma = 0
+
     for nota in notas:
         soma += nota
+
     media = soma / len(notas)
     print(f"A média das notas é: {media:.2f}")
-if __name__ == "__Main__":
+
+
+if __name__ == "__main__":
     exerc04()
