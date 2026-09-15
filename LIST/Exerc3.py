8c7241fc7433ea272aca43a0bef86f42002181f9
Videirafoo <videirafo@gmail.com>
2026-09-14 22:59:13 -0300

    fix: corrige execução individual do exercício 3

--- LIST/Exerc3.py
+++ LIST/Exerc3.py
@@ -1,18 +1,20 @@
 def exerc03():
-    
     print()
     print("3. Soma dos elementos")
     print("Crie uma lista com 8 números inteiros.")
-    print("Calcule e exiba a soma de todos os elementos ")
+    print("Calcule e exiba a soma de todos os elementos")
     print("sem utilizar a função sum().")
 
     print()
 
     numeros = [1, 2, 3, 4, 5, 6, 7, 8]
     soma = 0
-    for n in numeros:
-        soma += n
+
+    for numero in numeros:
+        soma += numero
+
     print(f"A soma dos elementos é: {soma}")
 
-if __name__ == "__Main__":
-    exec03()
+
+if __name__ == "__main__":
+    exerc03()
