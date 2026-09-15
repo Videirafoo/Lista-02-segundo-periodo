f590a7b7850f3365189e3e39f19ec7ef592f18b4
Videirafoo <videirafo@gmail.com>
2026-09-14 22:59:53 -0300

    fix: corrige execução individual do exercício 7

--- LIST/Exerc7.py
+++ LIST/Exerc7.py
@@ -1,22 +1,22 @@
 def exerc07():
     print()
-
-    print("7. Busca de um elemento ")
-    print("Leia 8 números e armazene-os em uma lista.") 
+    print("7. Busca de um elemento")
+    print("Leia 8 números e armazene-os em uma lista.")
     print("Depois, solicite ao usuário um número para pesquisar.")
     print("Informe se ele está ou não na lista. Não utilize index() para realizar a busca.")
-    
+
     print()
 
     numeros = []
     for i in range(8):
         numero = int(input(f"Digite o {i + 1}º número: "))
         numeros.append(numero)
-        
+
     numero_pesquisado = int(input("Digite o número a ser pesquisado: "))
     encontrado = False
-    for n in numeros:
-        if n == numero_pesquisado:
+
+    for numero in numeros:
+        if numero == numero_pesquisado:
             encontrado = True
             break
 
@@ -25,5 +25,6 @@ def exerc07():
     else:
         print(f"O número {numero_pesquisado} não está na lista.")
 
-if __name__ == "__Main__":
+
+if __name__ == "__main__":
     exerc07()
