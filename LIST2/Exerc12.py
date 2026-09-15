05fd1ea585dcaae0d19beb8f43da4b0ad2f00511
Videirafoo <videirafo@gmail.com>
2026-09-14 23:00:43 -0300

    fix: corrige execução individual do exercício 12

--- LIST2/Exerc12.py
+++ LIST2/Exerc12.py
@@ -1,20 +1,21 @@
-def exerc12 ():
-    
+def exerc12():
     print()
-   
     print("12. Lista sem repetição")
     print("Dada a lista numeros = [2, 5, 2, 8, 5, 9, 2, 8, 10],")
     print("crie uma nova lista contendo cada valor apenas uma vez,")
     print("mantendo a ordem da primeira ocorrência. Não utilize set().")
-    
+
     print()
-    
+
     numeros = [2, 5, 2, 8, 5, 9, 2, 8, 10]
     numeros_sem_repeticao = []
-    for n in numeros:
-        if n not in numeros_sem_repeticao:
-            numeros_sem_repeticao.append(n)
+
+    for numero in numeros:
+        if numero not in numeros_sem_repeticao:
+            numeros_sem_repeticao.append(numero)
+
     print("Lista sem repetição:", numeros_sem_repeticao)
 
-if __name__ == "__Main__":
+
+if __name__ == "__main__":
     exerc12()
