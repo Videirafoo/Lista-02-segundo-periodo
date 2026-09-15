23d7a7b6bd5c954c4c144ee935a22df13a23ef48
Videirafoo <videirafo@gmail.com>
2026-09-14 22:59:33 -0300

    fix: corrige execução individual do exercício 5

--- LIST/Exerc5.py
+++ LIST/Exerc5.py
@@ -1,19 +1,24 @@
 def exerc05():
-   
     print()
-    print("5. Maior e menor valor.")
-    print("Crie uma lista de números inteiros e determine o maior e o menor elemento sem utilizar max() ou min().")
-    
+    print("5. Maior e menor valor")
+    print("Crie uma lista de números inteiros e determine o maior e o menor elemento")
+    print("sem utilizar max() ou min().")
+
     print()
+
     numeros = [15, 22, 8, 19, 31, 5, 12]
     maior = numeros[0]
     menor = numeros[0]
+
     for numero in numeros:
         if numero > maior:
             maior = numero
         if numero < menor:
             menor = numero
+
     print(f"O maior valor da lista é: {maior}")
     print(f"O menor valor da lista é: {menor}")
-if __name__ == "__Main__":
+
+
+if __name__ == "__main__":
     exerc05()
